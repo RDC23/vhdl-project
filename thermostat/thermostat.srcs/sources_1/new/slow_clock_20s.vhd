e680688874748230d820dea3988e7f7cf473d6de
Ross Cathcart (Student) <ross.cathcart.2021@uni.strath.ac.uk>
2023-03-09 18:57:24 +0000

    created vivado project

---
+++ thermostat/thermostat.srcs/sources_1/new/slow_clock_20s.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09.03.2023 18:55:40
+-- Design Name: 
+-- Module Name: slow_clock_20s - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity slow_clock_20s is
+    Port ( clk : in STD_LOGIC;
+           clk_slow_20s : out STD_LOGIC);
+end slow_clock_20s;
+
+architecture Behavioral of slow_clock_20s is
+
+begin
+
+
+end Behavioral;
