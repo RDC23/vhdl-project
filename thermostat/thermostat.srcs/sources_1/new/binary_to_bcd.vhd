a4ebf36144e87036414da14e48a7aa730a8ecf04
Ross Cathcart <rdcscotland@outlook.com>
2023-03-12 13:04:16 +0000

    double dabble implemented. Needs tested

--- thermostat/thermostat.srcs/sources_1/new/binary_to_bcd.vhd
+++ thermostat/thermostat.srcs/sources_1/new/binary_to_bcd.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,15 +33,43 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity binary_to_bcd is
-    Port ( temp_in : in STD_LOGIC_VECTOR (5 downto 0);
+    --max 6 bit input (0-65 Celsius)
+    --two 4 bit outputs to hold digits from 0-9 for BCD
+    Port ( temp_in : in STD_LOGIC_VECTOR (5 downto 0); 
            temp_out_tens : out STD_LOGIC_VECTOR (3 downto 0);
            temp_out_ones : out STD_LOGIC_VECTOR (3 downto 0));
 end binary_to_bcd;
 
---Double dabble will be used to convert binary to bcd
 architecture Behavioral of binary_to_bcd is
 
 begin
+--sensitive to a change in input data
+convert process(temp_in):
+    variable intermediate : STD_LOGIC_VECTOR(5 downto 0);
+    variable scratch_space : unsigned(7 downto 0); --need to perfom arithmetic operations
+begin:
+    scratch_space := (others => '0');
+    intermediate := temp_in;
+    --n = input width iterations
+    for i in range 0 to 5 loop
+        --add 3 to each nibble of scratch space if it is >=5 to correct carry...
+        if scratch_space(3 downto 0) >= 5 then
+            scratch_space(3 downto 0) := scratch_space(3 downto 0) + 3;
+        end if;
 
+        if scratch_space(7 downto 4) >= 5 then
+            scratch_space(7 downto 4) := scratch_space(7 downto 4) + 3;
+        end if;
+
+        --shift the intermediate left into scratch space by 1 bit
+        scratch_space(7 downto 0) := scratch_space(6 downto 0) & intermediate(5);
+        intermediate := intermediate(4 downto 0) & '0';
+    
+    end loop;    
+    --push the respective portion of the scratch space into each BCD output digit
+    temp_out_tens <= STD_LOGIC_VECTOR(scratch_space(7 downto 4));
+    temp_out_ones <= STD_LOGIC_VECTOR(scratch_space(3 downto 0));
+
+end convert process;
 
 end Behavioral;
